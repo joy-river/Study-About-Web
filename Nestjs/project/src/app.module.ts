03efba45862c50b6b81e5cec76d78cb225e65b39
Joyriver-laptop <dlrkd1122@gmail.com>
2024-04-03 21:49:23 +0900

    DevKor_BE week 3

--- Nestjs/project/src/app.module.ts
+++ Nestjs/project/src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5432,
+      "username": "postgres",
+      "password": "postgres",
+      "database": "bestudy",
+      "autoLoadEntities": true,
+      "synchronize": true
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
